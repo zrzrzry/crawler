8440ee927d2792a8f4cc1637ba2bdc1fb9d76de9
902leader <493336365@qq.com>
2018-04-16 14:31:25 +0800

    servant.py

---
+++ Spider/Spider/servant.py
@@ -0,0 +1,8 @@
+class servant:
+    #从者基本属性类
+    name = ''#从者名
+    skill = []#从者技能
+    upgrade_material = []#进阶素材
+    skill_material = []#技能素材
+    #构造函数
+    def __init__(self, servant_name, servant_skill, servant_upgrade_material, servant_skill_material):
